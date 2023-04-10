80bcbb49b9f42495b33d17bab706154e56603bab
pedrosh101 <pedro.sh101@gmail.com>
2023-04-09 23:58:07 -0300

    mobile changes

--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-clr4 absolute z-50 w-full">
-      <div className="mx-auto py-6 px-4 sm:px-6 lg:px-28">
+      <div className="mx-auto py-3 lg:py-6 px-4 sm:px-6 lg:px-28">
         <div className="flex items-center justify-between h-16 font-avenirheavy">
           <div className="flex items-center">
             <Link href="/" legacyBehavior>
